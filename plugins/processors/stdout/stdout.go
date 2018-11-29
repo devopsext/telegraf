f965e0e0c49db51a3084b99a897f98c01ff917d3
Sergey Tomilov <sergey.tomilov@exness.com>
2018-11-29 08:58:46 +0200

    Added stdout

---
+++ plugins/processors/stdout/stdout.go
@@ -0,0 +1,69 @@
+package stdout
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"text/template"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/processors"
+)
+
+type Stdout struct {
+	template		*template.Template
+	Format      string `toml:"format"`
+}
+
+var sampleConfig = `
+`
+
+func (p *Stdout) SampleConfig() string {
+	return sampleConfig
+}
+
+func (p *Stdout) Description() string {
+	return "Print metrics that pass through this filter using predefined format."
+}
+
+func (p *Stdout) Apply(in ...telegraf.Metric) []telegraf.Metric {
+
+	if p.template == nil {
+
+		log.Printf("I! [processors.stdout] format: %s", p.Format)
+
+		t, err := template.New("").Parse(p.Format)
+		if err != nil {
+			log.Printf("E! [processors.stdout] could not create template: %v", err)
+			return in
+		}
+		p.template = t
+	}
+
+	for _, metric := range in {
+
+		m := make(map[string]interface{})
+		m["name"] = metric.Name()
+		m["time"] = metric.Time()
+		m["fields"] = metric.Fields()
+		m["tags"] = metric.Tags()
+
+		var b bytes.Buffer
+		err := p.template.Execute(&b, m)
+		if err == nil {
+	
+			fmt.Printf("%s\n", b.String())
+		}
+	}
+
+
+	return in
+}
+
+func init() {
+
+	processors.Add("stdout", func() telegraf.Processor {
+		return &Stdout{
+		}
+	})
+}
