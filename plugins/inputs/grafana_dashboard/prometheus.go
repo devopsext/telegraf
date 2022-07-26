32f8b03968ffecad5bd645846aefa2371ddc0f7a
Andrey Kolesnichenko <andrey.kolesnichenko@exness.com>
2022-07-26 17:45:21 +0300

    Query prometheus 1000 points per batch

--- plugins/inputs/grafana_dashboard/prometheus.go
+++ plugins/inputs/grafana_dashboard/prometheus.go
@@ -32,18 +32,22 @@ type Prometheus struct {
 }
 
 func (gp *Prometheus) GetData(t *sdk.Target, ds *sdk.Datasource, period *GrafanaDashboardPeriod, push GrafanaDatasourcePushFunc) error {
+	const gStep int64 = 60
+	const maxPoints int64 = 1000
 
 	params := make(url.Values)
 	params.Add("query", t.Expr)
 
 	t1, t2 := period.StartEnd()
-	start := int(t1.UTC().Unix())
-	end := int(t2.UTC().Unix())
+	start := t1.UTC().Unix()
+	end := t2.UTC().Unix()
 
-	params.Add("start", strconv.Itoa(start))
-	params.Add("end", strconv.Itoa(end))
+	pStep := gStep * maxPoints
 
-	params.Add("step", "60") // where it should be found?
+	params.Add("start", strconv.FormatInt(start, 10))
+	params.Add("end", strconv.FormatInt(end, 10))
+
+	params.Add("step", strconv.FormatInt(gStep, 10)) // where it should be found?
 	params.Add("timeout", gp.grafana.datasourceJSONValue(ds, "queryTimeout"))
 
 	customQueryParameters := gp.grafana.datasourceJSONValue(ds, "customQueryParameters")
@@ -56,62 +60,72 @@ func (gp *Prometheus) GetData(t *sdk.Target, ds *sdk.Datasource, period *Grafana
 		}
 	}
 
-	//gp.log.Debugf("Prometheus request params => %s", string(params))
+	for i := start; i < end; i += pStep {
+		s := i
+		e := i + pStep
+		if e > end {
+			e = end
+		}
+		params.Set("start", strconv.FormatInt(s, 10))
+		params.Set("end", strconv.FormatInt(e, 10))
 
-	when := time.Now()
+		gp.log.Debugf("querying prometheus from %s to %s", time.Unix(s, 0).Format(time.RFC3339), time.Unix(e, 0).Format(time.RFC3339))
 
-	URL := fmt.Sprintf("/api/datasources/proxy/%d/api/v1/query_range", ds.ID)
-	raw, code, err := gp.grafana.getData(ds, URL, params, nil)
-	if err != nil {
-		return err
-	}
-	if code != 200 {
-		return fmt.Errorf("Prometheus HTTP error %d: returns %s", code, raw)
-	}
-	var res PrometheusResponse
-	err = json.Unmarshal(raw, &res)
-	if err != nil {
-		return err
-	}
-	if res.Status != "success" {
-		return fmt.Errorf("prometheus status %s", res.Status)
-	}
-	if res.Data == nil {
-		gp.log.Debug("Prometheus has no data")
-		return nil
-	}
+		// gp.log.Debugf("Prometheus request params => %s", string(params))
 
-	for _, d := range res.Data.Result {
+		when := time.Now()
 
-		tags := make(map[string]string)
-		for k, m := range d.Metric {
-			tags[k] = m
+		gURL := fmt.Sprintf("/api/datasources/proxy/%d/api/v1/query_range", ds.ID)
+		raw, code, err := gp.grafana.getData(ds, gURL, params, nil)
+		if err != nil {
+			return err
+		}
+		if code != 200 {
+			return fmt.Errorf("prometheus HTTP error %d: returns %s", code, raw)
+		}
+		var res PrometheusResponse
+		err = json.Unmarshal(raw, &res)
+		if err != nil {
+			return err
+		}
+		if res.Status != "success" {
+			return fmt.Errorf("prometheus status %s", res.Status)
+		}
+		if res.Data == nil {
+			gp.log.Debug("Prometheus has no data")
+			return nil
 		}
 
-		for _, v := range d.Values {
-			if len(v) == 2 {
+		for _, d := range res.Data.Result {
+			tags := make(map[string]string)
+			for k, m := range d.Metric {
+				tags[k] = m
+			}
 
-				vt, ok := v[0].(float64)
-				if !ok {
-					continue
-				}
-				ts := int64(vt)
+			for _, v := range d.Values {
+				if len(v) == 2 {
+					vt, ok := v[0].(float64)
+					if !ok {
+						continue
+					}
+					ts := int64(vt)
 
-				vv, ok := v[1].(string)
-				if !ok {
-					continue
-				}
-				if f, err := strconv.ParseFloat(vv, 64); err == nil {
-					push(when, tags, time.Unix(ts, 0), f)
+					vv, ok := v[1].(string)
+					if !ok {
+						continue
+					}
+					if f, err := strconv.ParseFloat(vv, 64); err == nil {
+						push(when, tags, time.Unix(ts, 0), f)
+					}
 				}
 			}
 		}
 	}
+
 	return nil
 }
 
 func NewPrometheus(log telegraf.Logger, grafana *Grafana) *Prometheus {
-
 	return &Prometheus{
 		log:     log,
 		grafana: grafana,
